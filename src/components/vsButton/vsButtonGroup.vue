ec4d2d5fae20dfb31066cb58ad15b84b97149f97
Suraj S <suraj.sivakumar97@gmail.com>
2021-04-27 08:58:52 +0530

    - Add fixed height option for button groups with different content for uniformity

--- src/components/vsButton/vsButtonGroup.vue
+++ src/components/vsButton/vsButtonGroup.vue
@@ -1,5 +1,10 @@
 <template>
-	<div class="vs-button-group">
+	<div class="vs-button-group"
+	
+		:style="{
+			'--vs-bg-height': fixedHeight 
+		}"
+	>
 		<slot />
 	</div>
 </template>
@@ -9,35 +14,57 @@ import vsButton from "./vsButton.vue"
 
 
 export default defineComponent({
-	name: "VsButtonGroup"
+	name: "VsButtonGroup",
+	props: {
+		fixedHeight: {
+			type: String,
+			default: ""
+		}
+	}
 });
 </script>
 
-<style lang="sass">
+<style lang="scss">
 	  
-@import '../../style/sass/_mixins'
+@import '../../style/sass/_mixins';
 
-.vs-button-group
-  display: inline-flex
-  align-items: center
-  justify-content: center
-  margin: 5px
-  .vs-button
-    margin: 0px
-  .vs-button:not(:last-of-type):not(:first-of-type)
-    border-radius: 0px
-    border: 0px
-    &.vs-button--border
-      &:before
-        border-left: 0px
-        width: 100%
-      + .vs-button--border:not(:last-of-type)
-        &:before
-          border-right: 0px
-          width: calc(100%)
-  .vs-button:last-of-type
-    border-radius: 0px 12px 12px 0px
-  .vs-button:first-of-type
-    border-radius: 12px 0px 0px 12px
+.vs-button-group {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  margin: 5px;
+
+  .vs-button {
+    margin: 0px;
+	height: var(--vs-bg-height);
+  }
+
+  .vs-button:not(:last-of-type):not(:first-of-type) {
+    border-radius: 0px;
+    border: 0px;
+
+    &.vs-button--border {
+      &:before {
+        border-left: 0px;
+        width: 100%;
+      }
+
+      + .vs-button--border:not(:last-of-type) {
+        &:before {
+          border-right: 0px;
+          width: calc(100%);
+        }
+      }
+    }
+  }
+
+  .vs-button:last-of-type {
+    border-radius: 0px 12px 12px 0px;
+  }
+
+  .vs-button:first-of-type {
+    border-radius: 12px 0px 0px 12px;
+  }
+}
 
 </style>
