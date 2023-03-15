109a5c3f9de21994747d849bf5b69be00248c863
R Tushar <dmrtushar@gmail.com>
2023-03-15 10:14:01 +0530

    ~added tubutton tests

--- _tests/tuButton.spec.js
+++ _tests/tuButton.spec.js
@@ -1,14 +1,46 @@
-import { describe, vi, it, expect } from "vitest";
+import { describe, vi, it, expect, beforeEach } from "vitest";
 import { mount } from "@vue/test-utils";
 import { tuButton } from "../src/components/tuButton";
+import  Router  from "vue-router";
 
-describe("trigger events", () => {
-	it("should trigger and emit event on click", () => {
+vi.doMock("../src/components/tuComponent", () => {
+	const ComponentConstants = vi.importActual("../src/components/tuComponent");
+	return {
+		...ComponentConstants,
+		router : Router(() => ({
+			push:vi.fn()
+		}))
+	};
+
+});
+
+describe("tuButton", () => {
+	it("should trigger and emit event on click", async () => {
+		const wrapper = mount(tuButton);
+		await wrapper.find(".tu-button").trigger("click");
+		expect(wrapper.emitted("click")).toBeTruthy();
+	});
+	it("should trigger and emit event on mousedown", async () => {
+		const wrapper = mount(tuButton);
+		await wrapper.find(".tu-button").trigger("mousedown");
+		expect(wrapper.emitted("mousedown")).toBeTruthy();
+	});
+	it("disabled if activedisabled prop is passed", async () => {
 		const wrapper = mount(tuButton, {
 			props: {
-				success: true
+				activeDisabled: true
 			}
 		});
-		console.log(wrapper.html());
+		expect(wrapper.find(".tu-button--active-disabled")).toBeTruthy();
+	});
+	it("disabled if activedisabled prop is passed", async () => {
+		const wrapper = mount(tuButton, {
+			props: {
+				to: "/test"
+			}
+		});
+		let push = vi.fn();
+		await wrapper.find(".tu-button").trigger("click");
+		expect(push).toBeCalled();
 	});
 });
