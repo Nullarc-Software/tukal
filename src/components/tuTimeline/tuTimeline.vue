42cf7ba2d65e59eab2261a53546035907b174ecf
R Tushar <dmrtushar@gmail.com>
2023-03-26 10:06:14 +0530

    ~made few changes

--- src/components/tuTimeline/tuTimeline.vue
+++ src/components/tuTimeline/tuTimeline.vue
@@ -4,6 +4,7 @@
             <div class="tu-timeline-text" v-for="text in intervals" :class="{ 'tu-timeline-margin-left': text === '2am' }">{{ text }}</div>
         </div>
         <div class="tu-timeline-parent">
+            <div class="tu-timeline-current-time" :style="styleCurrentTime" ></div>
             <div class="tu-timeline-box" v-for="index in 12" :key="index" />
         </div>
     </div>
@@ -20,7 +21,16 @@ export default defineComponent({
     },
     setup() {
         let intervals = ["2am", "4am", "6am", "8am", "10am", "12pm", "2pm", "4pm", "6pm", "8pm", "10pm"]
-        return { intervals }
+        let styleCurrentTime = () => {
+            let hours = new Date().getHours();
+            let minutes = new Date().getMinutes();
+            let timeInMinutes = hours * 60 + minutes;
+            let percentage = (timeInMinutes / 24 * 60) * 100;
+            return {
+                "margin-left": percentage + "%"
+            }
+        }
+        return { intervals, styleCurrentTime }
     },
 })
 </script>
@@ -43,5 +53,11 @@ export default defineComponent({
 
 .tu-timeline-margin-left {
     margin-left: calc(100%/12);
+}
+
+.tu-timeline-current-time {
+    border-left: 3px dotted green;
+    height: 150px;
+    margin-top: -20px;
 }
 </style>
