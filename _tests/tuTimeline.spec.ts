b5bed820b943e3f59ecb4e626214c73b023bc962
R Tushar <dmrtushar@gmail.com>
2023-04-23 00:36:49 +0530

    ~updated the test cases

--- _tests/tuTimeline.spec.ts
+++ _tests/tuTimeline.spec.ts
@@ -1,13 +1,26 @@
-import { describe, it, expect} from "vitest";
+import { describe, it, expect, vi} from "vitest";
 import { mount } from "@vue/test-utils";
 import  tuTimeline  from "../src/components/tuTimeline";
 import { nextTick } from "vue";
+
 const hours = new Date().getHours();
 const minutes = new Date().getMinutes();
 const timeInMinutes = hours * 60 + minutes;
 const current = (timeInMinutes / 1440) * 100;
 
 const intervals = [1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1,1,0,1];
+ 
+const myElement = {
+	appendChild: vi.fn()
+};
+  
+const documentMock = {
+	getElementById: vi.fn().mockReturnValue(myElement)
+};
+
+const createElementSpy = vi.spyOn(document, "createElement");
+const getElementByIdSpy = vi.spyOn(document, "getElementById");
+
 describe("tuTimeline", () => {
 	it("should render current Time properly", async () => {
 		const wrapper = mount(tuTimeline, {
@@ -17,11 +30,30 @@ describe("tuTimeline", () => {
 				intervalEndTime: 19,
 				intervals: intervals
 			},
-			attachTo: document.body
+			attachTo: document.body,
+			globals: {
+				document: documentMock
+			}
 		});
 		await nextTick();
 		const currentTimeDiv = wrapper.find(".tu-timeline-current-time").element;
 		const marginLeft = window.getComputedStyle(currentTimeDiv).marginLeft;
 		expect(Math.round(parseFloat(marginLeft))).toBe(Math.round(current));
+	});
+	it("should append child divs", async () => {
+		const wrapper = mount(tuTimeline, {
+			props: {
+				interval: 5,
+				intervalStartTime: 9,
+				intervalEndTime: 19,
+				intervals: intervals
+			},
+			globals: {
+				document: documentMock
+			}
+		});
+		await nextTick();    
+		expect(createElementSpy).toHaveBeenCalledWith("div");
+		expect(getElementByIdSpy).toBeCalledWith("tu-timeline-box-1");
 	});
 });
